5cf79974b6f04f9df4b659f675c55981c321644b
gigclone <153342498+gigclone@users.noreply.github.com>
2023-12-16 13:25:47 +0700

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -265,8 +265,8 @@ iptables -A FORWARD -m string --algo bm --string "announce" -j DROP
 iptables -A FORWARD -m string --algo bm --string "info_hash" -j DROP
 iptables-save > /etc/iptables.up.rules
 iptables-restore -t < /etc/iptables.up.rules
-netfilter-persistent save
-netfilter-persistent reload
+#netfilter-persistent save
+#netfilter-persistent reload
 
 
 
