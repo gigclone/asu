84509d7363bc82d23a4777128d4eba48f99d9e04
gigclone <153342498+gigclone@users.noreply.github.com>
2023-12-18 15:11:46 +0700

    Update setupku.sh

--- setupku.sh
+++ setupku.sh
@@ -103,7 +103,7 @@ echo -e "$green      Install Websocket              $NC"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 sleep 2
 clear
-wget https://raw.githubusercontent.com/gigclone/asu/main/sshws/edu.sh && chmod +x edu.sh && ./edu.sh
+wget https://raw.githubusercontent.com/gigclone/asu/main/websocket/insshws.sh && chmod +x insshws.sh && ./insshws.sh
 
 #exp
 cd /usr/bin
